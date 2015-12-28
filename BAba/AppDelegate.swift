f0dbe48899837232957f6f2f011a02b0e82db42a
jeremyjousse <jeremy.jousse@coevo.fr>
2015-12-28 11:16:38 +0100

    First commit

---
+++ BAba/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  BAba
+//
+//  Created by Jérémy Jousse on 28/12/2015.
+//  Copyright © 2015 CoEvo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
---
+++ BAba/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BAba
+//
+//  Created by Jérémy Jousse on 28/12/2015.
+//  Copyright © 2015 CoEvo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
