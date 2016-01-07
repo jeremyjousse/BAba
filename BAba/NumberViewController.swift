5fde920f976724b057df1db4e14afeab77fed6f3
jeremyjousse <jeremy.jousse@coevo.fr>
2016-01-07 20:56:17 +0100

    Add NumberViewController

---
+++ BAba/NumberViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NumberViewController.swift
+//  BAba
+//
+//  Created by Jérémy Jousse on 07/01/2016.
+//  Copyright © 2016 CoEvo. All rights reserved.
+//
+
+import Cocoa
+
+class NumberViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+}
